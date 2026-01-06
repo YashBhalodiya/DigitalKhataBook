7e6e70f154d1517d73aba1c8e90013e41e97fcd4
YashBhalodiya <yashbhalodiya650@gmail.com>
2026-01-06 12:46:55 +0530

    feat: added logic for getting user profile in user service

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,6 +1,11 @@
-import { doc, setDoc } from "firebase/firestore"
-import { db } from "../../firebase"
+import { doc, getDoc, setDoc } from "firebase/firestore";
+import { db } from "../../firebase";
 
 export const createUserProfile = async (uid, userData) => {
     await setDoc(doc(db, "users", uid), userData)
 }
+
+export const getUserProfile = async (uid) => {
+  const snap = await getDoc(doc(db, "users", uid));
+  return snap.exists() ? snap.data() : null;
+};
