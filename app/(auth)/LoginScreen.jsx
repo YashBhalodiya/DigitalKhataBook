ee172c5b1960336bdbc2f28ea24cb4c76fac5d74
YashBhalodiya <yashbhalodiya650@gmail.com>
2026-01-07 20:27:11 +0530

    chore: moved the login code from src to app folder

---
+++ app/(auth)/LoginScreen.jsx
@@ -0,0 +1,175 @@
+import { useRouter } from "expo-router";
+import { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  ToastAndroid,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useAuth } from '../../src/context/AuthContext';
+
+const LoginScreen = () => {
+  const router = useRouter();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const {signInWithEmail} = useAuth();
+
+  const handleLogin = async () => {
+    try {
+      if (!email || !password) {
+        ToastAndroid.show("Please fill the fields", ToastAndroid.SHORT);
+        return;
+      }
+      setLoading(true);
+
+      await signInWithEmail(email, password);
+
+      router.replace("/(shop-owner)/Dashboard");
+
+      console.log("Logged in success");
+    } catch (error) {
+      console.log(error);
+    } finally{
+      setLoading(false);
+    }
+  };
+
+  const forgotPassword = () => {
+    console.log("Password forgot");
+  }
+
+  const handleCreateAccount = () => {
+    router.push("/(auth)/SignupScreen");
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.title}>Welcome Back</Text>
+        <Text style={styles.subTitle}>Please Login</Text>
+      </View>
+
+      <View style={styles.formContainer}>
+        <View style={styles.inputGroup}>
+          <Text style={styles.label}>Email</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Enter your email"
+            value={email}
+            onChangeText={setEmail}
+            keyboardType="email-address"
+            autoCapitalize="none"
+          />
+        </View>
+
+        <View style={styles.inputGroup}>
+          <Text style={styles.label}>Password</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Password"
+            value={password}
+            onChangeText={setPassword}
+            secureTextEntry
+          />
+        </View>
+
+        <TouchableOpacity style={styles.forgotPassButton} onPress={forgotPassword}>
+          <Text style={styles.forgotPassButtonText}>Forgot Passoword?</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.loginButton} onPress={handleLogin}>
+          <Text style={styles.loginButtonText}>Login</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={styles.createAccountButton}
+          onPress={handleCreateAccount}
+        >
+          <Text style={styles.createAccountText}>Don't have an account? Click here</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    paddingHorizontal: 20,
+    justifyContent: "flex-start",
+  },
+  header: {
+    marginBottom: 30,
+    marginTop: 50
+  },
+  title: {
+    fontSize: 35,
+    fontWeight: "600",
+    color: "#000",
+    marginBottom: 8,
+  },
+  subTitle: {
+    fontSize: 18,
+    color: "#686868ff",
+  },
+  formContainer: {
+    gap: 16,
+  },
+  inputGroup: {
+    gap: 8,
+  },
+  label: {
+    fontSize: 16,
+    color: "#000000ff",
+    paddingLeft: 4,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 14,
+    padding: 14,
+    fontSize: 16,
+    backgroundColor: "#fff",
+  },
+  forgotPassButton:{
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    marginVertical: 12
+  },
+  forgotPassButtonText: {
+    color: '#1eaa25ff',
+    fontSize: 15,
+    fontWeight: '500'
+  },
+  loginButton: {
+    backgroundColor: "#059669",
+    // borderWidth: 2,
+    // borderColor: "#000",
+    borderRadius: 14,
+    padding: 16,
+    alignItems: "center",
+    marginTop: 8,
+  },
+  loginButtonText: {
+    fontSize: 18,
+    fontWeight: "600",
+    color: "#ffffffff",
+  },
+  createAccountButton: {
+    alignItems: "center",
+    padding: 12,
+  },
+  createAccountText: {
+    fontSize: 14,
+    color: "#000",
+    // textDecorationLine: "underline",
+  },
+});
+
+export default LoginScreen;
---
+++ app/(auth)/SignupScreen.jsx
@@ -0,0 +1,265 @@
+import { Picker } from "@react-native-picker/picker";
+import { useRouter } from "expo-router";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { doc, setDoc } from "firebase/firestore";
+import { useState } from "react";
+import {
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  ToastAndroid,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { auth, db } from "../../firebase";
+
+const SignupScreen = () => {
+  const router = useRouter();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [mobile, setMobile] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [role, setRole] = useState("Customer");
+
+  const handleSignup = async () => {
+    try {
+      if (!name || !email || !mobile || !password || !confirmPassword) {
+        ToastAndroid.show("Fill the fields", ToastAndroid.SHORT);
+        return;
+      }
+
+      if (password !== confirmPassword) {
+        ToastAndroid.show("Passwords don't match", ToastAndroid.SHORT);
+        return;
+      }
+
+      if (mobile.length !== 10) {
+        ToastAndroid.show("Invalid mobile number", ToastAndroid.SHORT);
+        return;
+      }
+
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+
+      const user = userCredential.user;
+      console.log("User Created:", user.uid);
+
+      await setDoc(doc(db, "users", user.uid), {
+        uid: user.uid,
+        name: name,
+        email: email,
+        mobile: mobile,
+        role: role,
+        createdAt: new Date().toISOString(),
+      });
+      ToastAndroid.show("Account created successfully!", ToastAndroid.SHORT);
+      router.replace("/(auth)/LoginScreen");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleBackToLogin = () => {
+    router.back();
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <KeyboardAvoidingView
+        style={styles.keyboardView}
+        keyboardVerticalOffset={Platform.OS === "android" ? 0 : 20}
+        behavior={Platform.OS === "android" ? "padding" : "height"}
+      >
+        <ScrollView
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={styles.scrollContent}
+          keyboardShouldPersistTaps="handled"
+        >
+          <View style={styles.header}>
+            <Text style={styles.title}>Create Account</Text>
+            <Text style={styles.subtitle}>Sign up to get started</Text>
+          </View>
+
+          <View style={styles.formContainer}>
+            <View style={styles.inputGroup}>
+              <Text style={styles.label}>Full Name</Text>
+              <TextInput
+                style={styles.input}
+                placeholder="Enter your name"
+                value={name}
+                onChangeText={setName}
+              />
+            </View>
+
+            <View style={styles.inputGroup}>
+              <Text style={styles.label}>Mobile Number</Text>
+              <TextInput
+                style={styles.input}
+                placeholder="Enter mobile number"
+                value={mobile}
+                onChangeText={setMobile}
+                keyboardType="phone-pad"
+              />
+            </View>
+
+            <View style={styles.inputGroup}>
+              <Text style={styles.label}>Select Role</Text>
+
+              <View style={styles.pickerWrapper}>
+                <Picker
+                  selectedValue={role}
+                  onValueChange={(itemValue) => setRole(itemValue)}
+                  style={styles.picker}
+                >
+                  <Picker.Item label="Customer" value="customer" />
+                  <Picker.Item label="Shop Owner" value="shop-owner" />
+                </Picker>
+              </View>
+            </View>
+
+            <View style={styles.inputGroup}>
+              <Text style={styles.label}>Email</Text>
+              <TextInput
+                style={styles.input}
+                placeholder="Enter your email"
+                value={email}
+                onChangeText={setEmail}
+                keyboardType="email-address"
+                autoCapitalize="none"
+              />
+            </View>
+
+            <View style={styles.inputGroup}>
+              <Text style={styles.label}>Password</Text>
+              <TextInput
+                style={styles.input}
+                placeholder="Create password"
+                value={password}
+                onChangeText={setPassword}
+                secureTextEntry
+              />
+            </View>
+
+            <View style={styles.inputGroup}>
+              <Text style={styles.label}>Confirm Password</Text>
+              <TextInput
+                style={styles.input}
+                placeholder="Confirm password"
+                value={confirmPassword}
+                onChangeText={setConfirmPassword}
+                secureTextEntry
+              />
+            </View>
+
+            <TouchableOpacity
+              style={styles.signupButton}
+              onPress={handleSignup}
+            >
+              <Text style={styles.signupButtonText}>Create Account</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity
+              style={styles.loginButton}
+              onPress={handleBackToLogin}
+            >
+              <Text style={styles.loginText}>
+                Already have an account? Login
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </ScrollView>
+      </KeyboardAvoidingView>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    paddingHorizontal: 20,
+    paddingTop: 35,
+  },
+  keyboardView: {
+    flex: 1,
+  },
+  scrollContent: {
+    flexGrow: 1,
+  },
+  header: {
+    marginBottom: 30,
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: "600",
+    color: "#000",
+    marginBottom: 8,
+  },
+  subtitle: {
+    fontSize: 16,
+    color: "#666",
+  },
+  formContainer: {
+    gap: 16,
+    paddingRight: 1
+  },
+  inputGroup: {
+    gap: 8,
+  },
+  label: {
+    fontSize: 16,
+    color: "#000000ff",
+    paddingLeft: 4,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 14,
+    padding: 14,
+    fontSize: 16,
+    backgroundColor: "#fff",
+  },
+  pickerWrapper: {
+    borderWidth: 1,
+    borderRadius: 14,
+    borderColor: "#ccc",
+    paddingHorizontal: 6
+  },
+  picker: {
+    height: 53,
+    color: '#575757ff'
+  },
+  signupButton: {
+    backgroundColor: "#059669",
+    // borderWidth: 2,
+    // borderColor: "#000",
+    borderRadius: 14,
+    padding: 16,
+    alignItems: "center",
+    marginTop: 8,
+  },
+  signupButtonText: {
+    fontSize: 18,
+    fontWeight: "600",
+    color: "#ffffffff",
+  },
+  loginButton: {
+    alignItems: "center",
+    padding: 12,
+  },
+  loginText: {
+    fontSize: 14,
+    color: "#000",
+    // textDecorationLine: "underline",
+  },
+});
+
+export default SignupScreen;
