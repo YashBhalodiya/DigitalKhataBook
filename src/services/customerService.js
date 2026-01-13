a093693506b7a5cf25251135054dc91612ba7536
YashBhalodiya <yashbhalodiya650@gmail.com>
2026-01-13 22:55:28 +0530

    feat(customerService): setup services for customers

---
+++ src/services/customerService.js
@@ -0,0 +1,47 @@
+import { collection, doc, getDocs, query, serverTimestamp, setDoc, where } from "firebase/firestore";
+import { db } from "../../firebase";
+
+export const customerService = {
+  addCustomer: async (shopId, customerData) => {
+    try {
+      const customerRef = doc(collection(db, 'customers'));
+      const customerId = customerRef.id;
+
+      await setDoc(customerRef,{
+        shopId: shopId,
+        name: customerData.name,
+        phone: customerData.phone,
+        totalDue: customerData.totalDue || 0,
+        createdAt: serverTimestamp(),
+        updatedAt: serverTimestamp()
+      })
+      return customerId;
+    } catch (error) {
+      throw error
+    }
+  },
+  getCustomers: async (shopId) => {
+    try {
+      const customerRef = collection(db, 'customers')
+      const q = query(customerRef, where('shopId', '==', shopId))
+      const querySnapshot = await getDocs(q)
+      const customers = []
+      querySnapshot.forEach((doc) => {
+        customers.push({id: doc.id, ...doc.data()})
+      })
+      return customers
+    } catch (error) {
+      throw error
+    }
+  },
+  getCustomerCount: async (shopId) => {
+    try {
+      const customerRef = collection(db, 'customers')
+      const q = query(customerRef, where('shopId', '==', shopId))
+      const querySnapshot = await getDocs(q)
+      return querySnapshot.size
+    } catch (error) {
+      throw error
+    }
+  }
+};
